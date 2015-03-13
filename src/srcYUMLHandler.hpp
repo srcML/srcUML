45abbf0ae90c6e141c4a0307668dfae032f437cc
Michael John Decker <mdecker6@kent.edu>
2015-03-13 17:21:34 -0400

    Update copyright.

--- src/srcYUMLHandler.hpp
+++ src/srcYUMLHandler.hpp
@@ -1,14 +1,14 @@
 /**
- * @file srcSAXHandler.hpp
+ * @file srcYUMLHandler.hpp
  *
- * @copyright Copyright (C) 2013-2014 srcML, LLC. (www.srcML.org)
+ * @copyright Copyright (C) 2014 srcML, LLC. (www.srcML.org)
  *
- * srcSAX is free software; you can redistribute it and/or modify
+ * srcYUML is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * srcSAX is distributed in the hope that it will be useful,
+ * srcYUML is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
