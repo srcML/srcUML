f39a99295fee402874d1e7eb6d07573c31dd3610
PJ-Leyden <pjleyden.mist31@gmail.com>
2016-06-13 16:01:24 -0400

    Handle unicode input.

--- src/srcYUML2graphViz/srcYUML2graphViz.py
+++ src/srcYUML2graphViz/srcYUML2graphViz.py
@@ -2,9 +2,13 @@ from antlr4 import *
 from antlr4.tree.Trees import Trees
 from srcYUML2graphVizLexer import srcYUML2graphVizLexer
 from srcYUML2graphVizParser import srcYUML2graphVizParser
+from kitchen.text.converters import to_unicode
 
 def main(argv):
-    input = FileStream(argv[1])
+    file = open(argv[1], "rb")
+    input_str = to_unicode(file.read())
+    file.close()
+    input = InputStream(input_str)
     lexer = srcYUML2graphVizLexer(input)
     stream = CommonTokenStream(lexer)
     parser = srcYUML2graphVizParser(stream)
