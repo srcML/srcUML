43c01d6a210211092a09394c06c5597de3a96ed0
Michael John Decker <mdecker6@kent.edu>
2016-06-30 15:55:45 -0400

    Default initialize variables in srcYUMLClass.hpp

--- src/srcYUMLClass.hpp
+++ src/srcYUMLClass.hpp
@@ -196,9 +196,14 @@ public:
     // To class as our key, so we have unique relationships for each class
     std::map<std::string, Relationship> relationships_;
     
-    srcYUMLClass() {}
+    srcYUMLClass() : srcYUMLClass("") {}
 
-    srcYUMLClass(std::string class_name) : class_name_(class_name) {}
+    srcYUMLClass(const std::string & class_name) : is_interface(false),
+                     is_data_type(false),
+                     default_constructor(false),
+                     copy_constructor(false),
+                     overloaded_assignment(false),
+                     class_name_(class_name) {}
 
     void setClassName(std::string class_name) {
         class_name_ = class_name;
