60882ee9b9cb483939d608693aec83698d253416
PJ-Leyden <pleyden@kent.edu>
2017-08-07 17:16:37 -0400

    start layout engine, yUML_to_DOT tests

--- src/layout_engine/src/main_test.cpp
+++ src/layout_engine/src/main_test.cpp
@@ -1,19 +1,28 @@
 /*
 Author: P.J. Leyden
 File: main_test.cpp
-
-compile with clang++ main_test.cpp --std=c++11 -I/home/wolfdragoon31/Documents/Programming/srcUML/ogdf/include/
-
+To Compile:
+	clang++ main_test.cpp -std=c++11 -I/home/wolfdragoon31/Documents/Programming/ogdf/include/ -Xlinker ../../../../ogdf/libOGDF.a
 */
 #include <ogdf/basic/Graph.h>
 #include <ogdf/basic/Graph_d.h>
 #include <ogdf/basic/GraphAttributes.h>
 #include <ogdf/basic/graph_generators.h>
+#include <string>
 
 int main(){
 
 	ogdf::Graph my_graph;
 
+	int index = 0;
+
+	//returns a node element
+	
+	
+	my_graph.newNode(index);
+
+
+	std::cout << my_graph.empty() << std::endl;
 
 	return 0;
 }
