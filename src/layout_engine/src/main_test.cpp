e40c919fba8482e10680a38b092387e86532e056
PJ-Leyden <pleyden@kent.edu>
2017-11-14 12:00:27 -0500

    testing main

--- src/layout_engine/src/main_test.cpp
+++ src/layout_engine/src/main_test.cpp
@@ -2,59 +2,87 @@
 Author: P.J. Leyden
 File: main_test.cpp
 To Compile:
-	clang++ main_test.cpp -D NDEBUG -std=c++11 -Xlinker ../../../../ogdf/libOGDF.a
+	clang++ main_test.cpp -D NDEBUG -std=c++11 -oogdf_test -Xlinker ../../../../ogdf/libOGDF.a
 */
+//Basic_Graph_Include================================================
 #include <ogdf/basic/Graph.h>
 #include <ogdf/basic/Graph_d.h>
 #include <ogdf/basic/GraphAttributes.h>
 #include <ogdf/basic/graph_generators.h>
+//===================================================================
 
+//Cluster_Graph_Include==============================================
+#include <ogdf/cluster/ClusterGraph.h>
+#include <ogdf/cluster/ClusterGraphAttributes.h>
+//===================================================================
+
+//Input_Output_Include===============================================
 #include <ogdf/fileformats/GraphIO.h>
+//===================================================================
 
+//Layout_Include=====================================================
 #include <ogdf/tree/TreeLayout.h>
 #include <ogdf/misclayout/CircularLayout.h>
 #include <ogdf/module/UMLLayoutModule.h>
+//===================================================================
 
-#include <ogdf/uml/UMLGraph.h>
-#include <ogdf/uml/PlanarizationLayoutUML.h>
-
+//Basic_Include======================================================
 #include <string>
 #include <iostream>
+//===================================================================
+
 
 int main(int argc, char ** argv) {
 
-	//create a pointer to a graph
-	ogdf::Graph * my_graph = new ogdf::Graph();
 
-	//create the attributes class and associate it with my_graph and turn on all attr lists
-	ogdf::UMLGraph * uml_graph = new ogdf::UMLGraph(*my_graph, 0x1FFFF);
+	//Create Objects=================================================
+	//Create graph pointer
+	ogdf::Graph * base_graph = new ogdf::Graph();
+
+	//created cluster graph and asscoiate base_graph with it. 
+	ogdf::ClusterGraph * cluster_graph = new ogdf::ClusterGraph(*base_graph);
+
+	//create graph and attributes holder. Main Object
+	ogdf::ClusterGraphAttributes * cluster_graph_attr = new ogdf::ClusterGraphAttributes(*cluster_graph, 0x1FFFF);
+
+	//create svg settings object
+	ogdf::GraphIO::SVGSettings * svg_settings = new ogdf::GraphIO::SVGSettings();
 
 	//get dot file
 	std::ifstream dot_file(argv[1]);
 
 	//read in a dot formated file and store in appropriate lists and objects
-	if(!ogdf::GraphIO::readDOT(*uml_graph, *my_graph, dot_file))
+	if(!ogdf::GraphIO::readDOT(*cluster_graph_attr, *cluster_graph, *base_graph, dot_file)){
 		std::cout << "Error Read" << std::endl;
+	}
+	//===============================================================
 
+
+	//Layout_Call====================================================
 	//create a circular layout objects
-	ogdf::CircularLayout * my_circle = new ogdf::CircularLayout();
+	ogdf::CircularLayout * circle_layout = new ogdf::CircularLayout();
 
 	//run on graph
-	my_circle->call(*uml_graph);
-	
-	//output file
+	circle_layout->call(*cluster_graph_attr);
+	//===============================================================
+
+
+	//Output_Call====================================================
+	//Output file
 	std::ostream * out;
 	if(argc > 2){
 		out = new std::ofstream(argv[2]);
+		std::cout << "Printing to: " << argv[2] << std::endl;
 	}else{
 		out = &std::cout;
 	}
 
-	if(!ogdf::GraphIO::drawSVG(*uml_graph, *out))
+	//call draw function
+	if(!ogdf::GraphIO::drawSVG(*cluster_graph_attr, *out, *svg_settings)){
 		std::cout << "Error Write" << std::endl;
+	}
+	//===============================================================
+
 
 	return 0;
-
 }
-
-
