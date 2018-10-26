7560996a55e25eb031e5bda843aec19410d95ae0
PJ-Leyden <pleyden@kent.edu>
2018-10-25 23:49:03 -0400

    Edited CMake files for compiling and brought srcuml file up to test point

--- src/generator/ogdf_outputter.hpp
+++ src/generator/ogdf_outputter.hpp
@@ -11,14 +11,40 @@
 #include <srcuml_outputter.hpp>
 
 //OGDF_Requirements
+//Basic_Graph_Include================================================	
 #include <ogdf/basic/Graph.h>
 #include <ogdf/basic/Graph_d.h>
 #include <ogdf/basic/GraphAttributes.h>
 #include <ogdf/basic/graph_generators.h>
+//===================================================================
+
+//Cluster_Graph_Include==============================================
 #include <ogdf/cluster/ClusterGraph.h>
 #include <ogdf/cluster/ClusterGraphAttributes.h>
+//===================================================================
+
+//Input_Output_Include===============================================
+#include <ogdf/fileformats/GraphIO.h>
+//===================================================================
+
+//Layout_Include=====================================================
+#include <ogdf/tree/TreeLayout.h>
+#include <ogdf/misclayout/CircularLayout.h>
+#include <ogdf/module/UMLLayoutModule.h>
+//===================================================================
+
+//Layered============================================================
+#include <ogdf/layered/SugiyamaLayout.h>
+#include <ogdf/layered/OptimalRanking.h>
+#include <ogdf/layered/MedianHeuristic.h>
+#include <ogdf/layered/OptimalHierarchyLayout.h>
+#include <ogdf/layered/OptimalHierarchyClusterLayout.h>
+#include <ogdf/module/HierarchyClusterLayoutModule.h>
+//===================================================================
+
 
 using namespace ogdf;
+using namespace ogdf::internal;
 
 class ogdf_outputter : srcuml_outputter{
 
@@ -38,13 +64,13 @@ public:
 	bool output(std::ostream& out, std::vector<std::shared_ptr<srcuml_class>> & classes){
 		//transfer information from srcUML to OGDF
 		srcuml_relationships relationships = analyze_relationships(classes);
-		/*std::map<std::string, ogdf::node&> class_node_map;
+		std::map<std::string, ogdf::node&> class_node_map;
 
 		//Classes/Nodes
 		for(const std::shared_ptr<srcuml_class> & aclass : classes){
 			ogdf::node cur_node = g.newNode();
 			//Insert into map the node class pairing
-			class_node_map.insert(std::pair<std::string, ogdf::node&>(aclass.get_name(), cur_node));
+			class_node_map.insert(std::pair<std::string, ogdf::node&>(aclass->get_name(), cur_node));
 			//Set Label
 			std::string& label = ga.label(cur_node);
 			label = generate_label(aclass);
@@ -52,15 +78,54 @@ public:
 			double& w = ga.width(cur_node);
 			double& h = ga.height(cur_node);
 			//Set Color
-			Color& color = ga.fillColor(cur_node);*/
+			Color& color = ga.fillColor(cur_node);
+		}
 
 
 		//Relationships/Edges
 		for(const srcuml_relationship relationship : relationships.get_relationships()){
 			//get the nodes from graph g, create edge and add apropriate info.
 			//std::cout << "Taco in ogdf\n";
-			//ogdf::edge& cur_edge = g.newEdge(class_node_map.find(relationship.get_source())->second, class_node_map.find(relationship.get_destination())->second);//need to pass to ogdf::node types
+			ogdf::node lhs = class_node_map.find(relationship.get_source())->second;
+			ogdf::node rhs = class_node_map.find(relationship.get_destination())->second;
+			ogdf::edge cur_edge = g.newEdge(lhs, rhs);//need to pass to ogdf::node types
 		}
+
+
+		for(EdgeElement * e = g.firstEdge(); e; e = e->succ()){
+			float& w = ga.strokeWidth(e);
+			w = 1;
+		}
+
+		for(NodeElement * n = g.firstNode(); n; n = n->succ()){
+			double& h = ga.height(n);
+			double& w = ga.width(n);
+			Color& color = ga.fillColor(n);
+
+			h = 120;
+			w = 120;
+			color = Color(Color::Name::Antiquewhite);
+		}
+
+
+		SugiyamaLayout sl;
+		sl.setRanking(new OptimalRanking);
+		sl.setCrossMin(new MedianHeuristic);
+ 
+		OptimalHierarchyLayout *ohl = new OptimalHierarchyLayout;
+		ohl->layerDistance(30.0);
+		ohl->nodeDistance(25.0);
+		ohl->weightBalancing(0.8);
+		sl.setLayout(ohl);
+
+		sl.call(ga);
+
+		GraphIO::SVGSettings * svg_settings = new ogdf::GraphIO::SVGSettings();
+
+		if(!ogdf::GraphIO::drawSVG(ga, out, *svg_settings)){
+			std::cout << "Error Write" << std::endl;
+		}
+
 
 		return true;
 	}
