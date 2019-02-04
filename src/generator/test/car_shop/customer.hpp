22bf0fe23f6e0908579dd98f84455231163d7048
PJ-Leyden <pleyden@kent.edu>
2019-02-03 22:06:50 -0500

    Added start of testing files

---
+++ src/generator/test/car_shop/customer.hpp
@@ -0,0 +1,13 @@
+#include <string>
+
+class Customer{
+
+private:
+	std::string name;
+	std::string address;
+	std::string credit_card;
+
+public:
+
+
+}
---
+++ src/generator/test/car_shop/employee.hpp
@@ -0,0 +1,11 @@
+#include "customer.hpp"
+#include "shop.hpp"
+
+class Employee : Customer{
+private:
+	int employee_id;
+	shop *shop_of_work;
+
+public:
+
+}
